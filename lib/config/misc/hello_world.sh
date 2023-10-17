79205be46843811564ba7c4e7a3cd8c806d3c5ce
Joey Turner <tearran@g....com>
2023-10-17 05:46:41 +0000

    modified:   lib/config/misc/hello_world.sh

--- lib/config/misc/hello_world.sh
+++ lib/config/misc/hello_world.sh
@@ -12,53 +12,58 @@
 #
 # @stdout A Here Doc Armbian logo
 function hello_config::ascii_animation() {
-    # Define arrays for frames and associated text
-    frames=(
-        ")"
-        "_■)"
-        "(⌐■_■)"
-        "( •_•)>⌐■-■"
-        " (•_•)"
-        "(•_•)"
-        "_•)"
-        "•)"
-        " "
-        "•) "
-        "_•)"
-        ")"
-    )
+# Define arrays for frames, associated text, and commands
+frames=(
+    ")"
+    "_■)"
+    "(⌐■_■)"
+    "( •_•)>⌐■-■"
+    " (•_•)"
+    "(•_•)"
+    "_•)"
+    "•)"
+    " "
+    "•) "
+    "_•)"
+    ")"
+)
 
-    text=(
-        " "    
-        " "
-        "Oh, hello."
-        "This Hello Library example"
-        "Copy the misc folder and its contents with a new name"
-        "Rename new folder and about_misc.sh to local/about_local.sh"
-        "Edit the about_local.sh  to \n# @description language, keyboard, and time zone."
-        "This will create a new category entry in the main menu\nThe about will add the new subcategory menu"
-        "\n\nThank you for watching\n"
-        ""
-    )
+text=(
+    " "    
+    " "
+    "Oh, hello."
+    "This Hello Library example"
+    "Copy the misc folder and its contents with a new name"
+    "Rename new folder and about_misc.sh to local/about_local.sh"
+    "Edit the about_local.sh  to \n# @description language, keyboard, and time zone."
+    "This will create a new category entry in the main menu\nThe about will add the new subcategory menu"
+    "\n\nThank you for watching\n"
+    ""
+)
 
-    # Loop through frames
-    clear
-    for i in "${!frames[@]}"; do
-        echo -e "\e[H\e[K ${frames[i]}"
-        echo -e "\e[K ${text[i]}"
-        sleep 2
-    done
-    echo -en "\e[999;1H"
-    art_armbian
-    
+commands=(
+    ""
+    ""
+    ""
+    ""
+    ""
+    # Add more commands as needed
+)
+
+# Loop through frames
+clear
+for i in "${!frames[@]}"; do
+    echo -e "\e[H \n\n\n \e[K ${frames[i]}"
+    echo -e "\n\n\e[K ${text[i]}\n\n\n\n"
+    eval "${commands[i]}"  # Execute the command
+    sleep 2
+done
+
+    echo -en "\e[999;1H"    
 }
 
 art_armbian() {   
-    cat << EOF
-
-
-
-    
+    cat << EOF    
       █ █ █ █ █ █ █ █ █ █ █
      ███████████████████████
    ▄▄██                   ██▄▄
